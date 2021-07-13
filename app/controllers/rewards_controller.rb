66d2b82acf1a235e4debf6b540a5e4e024451d6a
neko <neko@MyTUFLaptop.localdomain>
2021-07-13 14:45:19 +1000

    Modified Rewards controller

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,42 @@
 class RewardsController < ApplicationController
+  #  before_action :authenticate
+  before_action :set_reward, only: %i[show create update destroy]
+
+  def index
+    render json: Reward.all
+  end
+
+  def show
+    render json: @reward
+  end
+
+  def create
+    render json: Reward.create(reward_params), status: :created
+  end
+
+  def update
+    if @reward.update(reward_params)
+      render json: @reward
+    else
+      render json: { error: 'Failed to update reward' }
+    end
+  end
+
+  def destroy
+    if @reward.destroy
+      render status: :no_content
+    else
+      render json: { error: 'Failed to destroy reward' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_reward
+    @reward = Reward.find(params[:id])
+  end
+
+  def rewards_params
+    params.permit(:param1, :param2)
+  end
 end
