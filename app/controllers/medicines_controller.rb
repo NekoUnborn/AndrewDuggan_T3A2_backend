a66ea48fc88fb3ddffcf8b7d66c23ddc344e9226
Timothy Moss <tim@timothys-mbp.lan>
2021-07-23 16:37:40 +1000

    Removed redundant controller actions within the medcines controller on api backend

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -21,31 +21,6 @@ class MedicinesController < ApplicationController
         render json: {message: @new_med.errors.full_messages}, status: 401
       end
     end 
-    # Accepts the given parameters
-    # Checks that the DB does not already have an existing entry
-    # If the entry does not already exist, then create the new Medicine entry.
-    # case Medicine.where(name: medicine_params[:name])
-    #  when true
-    # Return a status message
-    # when false 
-    # return a json create method (below line works)
-    # render json: Medicine.create(medicine_params), status: :created         
-  end
-
-  def update
-    if @medicine.update(medicine_params)
-      render json: @medicine
-    else
-      render json: { error: 'Failed to update medicine' }
-    end
-  end
-
-  def destroy
-    if @medicine.destroy
-      render status: :no_content
-    else
-      render json: { error: 'Failed to destroy medicine' }, status: :unprocessable_entity
-    end
   end
 
   private
